3b881a508407475a9e0f3b5d088b4dc265dcce4e
carsonadair <carsonadairphoto@gmail.com>
2023-04-09 20:15:02 -0400

    built classes in lib and started requirements in indexjs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Html generator will go here
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+
+const Directory = path.resolve(__dirname, "dist");
+const distPathway = path.join(Directory, "index.html");
+
+const generatefile = require("./src/generatefile.js");
